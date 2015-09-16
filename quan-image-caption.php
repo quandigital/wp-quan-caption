bf32c4036b4b91e2948f86c4f649142c3a189bee
alpipego <alpipego@gmail.com>
2015-09-16 18:43:11 +0200

    cosmetic changes, version number, plugin url; added author, author uri

--- quan-image-caption.php
+++ quan-image-caption.php
@@ -1,31 +1,33 @@
 <?php
 
-    /**
-     *   Plugin Name: Quan Image Caption
-     *   Plugin URI: http://www.quandigital.com/
-     *   Description: Adds a nicer image/caption shortode
-     *   Version: 1.0.0
-     *   License: MIT
-     */
-    
-    defined( 'ABSPATH' ) or die();
+/**
+ *   Plugin Name: Quan Image Caption
+ *   Plugin URI: https://github.com/quandigital/wp-quan-caption
+ *   Author: Quan Digital GmbH
+ *   Author URI: http://www.quandigital.com/
+ *   Description: Adds a nicer image/caption shortode
+ *   Version: 1.0.1
+ *   License: MIT
+ */
 
-    function quanImageCaption($atts, $content = null)
-    {
-        if (isset($atts['width'])) {
-            $width = $atts['width'] == 100 ? 'full' : 'half';
-        } else {
-            $width = 'full';
-        }
+defined('ABSPATH') or die();
 
-        if (isset($atts['src'])) {
-            if (!is_null($content)) {
-                return '<div class="content-image ' . $width . '"><img src="' . $atts['src'] . '" /><div class="caption">' . $content . '</div></div>';
-            }
-            return '<div class="content-image ' . $width . '"><img src="' . $atts['src'] . '" /></div>';
-        } 
-
-        return;
+function quanImageCaption($atts, $content = null)
+{
+    if (isset($atts['width'])) {
+        $width = $atts['width'] == 100 ? 'full' : 'half';
+    } else {
+        $width = 'full';
     }
 
-    add_shortcode( 'caption', 'quanImageCaption' );
+    if (isset($atts['src'])) {
+        if (!is_null($content)) {
+            return '<div class="content-image ' . $width . '"><img src="' . $atts['src'] . '" /><div class="caption">' . $content . '</div></div>';
+        }
+        return '<div class="content-image ' . $width . '"><img src="' . $atts['src'] . '" /></div>';
+    } 
+
+    return;
+}
+
+add_shortcode('caption', 'quanImageCaption');
